dfc60567cd7faf6def84a8517879db058606b165
N-Kulagin <theg18glock@mail.ru>
2021-09-09 12:05:10 +0300

    Добавьте файлы проекта.

---
+++ Дискриминантная функция/Excel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace Дискриминантная_функция
+{
+    class Excel
+    {
+        public Excel(string path, string sheet)
+        {
+
+        }
+    }
+}
---
+++ Дискриминантная функция/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NKulaign.Matrices;
+
+namespace Дискриминантная_функция
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
